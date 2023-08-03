5fa091d879b2df3f4d5b862fe70b6832f003c53f
“Laibah <ashfaq.laibah@gmail.com>
2023-08-03 07:09:55 -0400

    adding dashboard

--- AAA test/app.py
+++ AAA test/app.py
@@ -28,64 +28,40 @@ from tsfresh import select_features
 from tsfresh.utilities.dataframe_functions import impute
 from tsfresh import extract_relevant_features
 from tsfresh.transformers import RelevantFeatureAugmenter
-
-
-
-
+import umap
+import pickle
 
 
 st.title('Epilepsy Classification')
-
-
 st.header('upload CSV file')
 
 uploaded_file = st.file_uploader('Upload a file')
 
-
-
-
-
-
-if st.button('create data'):
-        pred_data = pd.read_csv(uploaded_file)
-        y = pd.read_csv('y_for_modeling.csv')
-        X = pd.read_csv('Data/extracted_features.csv')
-
-        pred_data = extract_relevant_features(X, y , column_id = "Unnamed", column_sort='Time', column_value="Value").fit_transform(pred_data, pred_data['y'])
-
+# Load Model
+loaded_model = pickle.load(open("pca_logreg_model.pickle", "rb"))
         
-y = pd.read_csv('data/y_for_modeling.csv')
-
-X = pd.read_csv('data/extracted_features.csv')
-
-
-
-
-
-
-
-#df_X_features.to_csv('Data/extracted_features.csv')
-
-
-
-
-
-
-
-
-
-
-#X.rename(columns={"Unnamed: 0": 'Segment'}, inplace=True)
-
-model = LogisticRegression(random_state=42)
-
 if st.button('predict'):
-    model.fit(X, y)
+    pred_data = pd.read_csv(uploaded_file)
+    
+    #Plot this data     
+    original_data = pd.read_csv('data/Seizure_data.csv')
+    row_to_plot = original_data[original_data['Unnamed'] == pred_data['Unnamed'][0]]
+    fig, ax = plt.subplots(figsize=(12, 5))
+    plt.xticks(rotation=45, ha='right', fontsize = 0)
+    # Plot the row data
+    plt.plot(row_to_plot.T.values[1:])
+    plt.xlabel('Time', fontsize = 15)
+    plt.ylabel('Value of EEG scan')
+    plt.title('EEG Scan')
+    st.pyplot(fig)
 
-
-
-    y_pred = model.predict(pred_data)
-    if y_pred ==1:
-        st.write("you're epileptic AF")
+    
+    #Predict this data
+    feature_data = pd.read_csv('data/extracted_features.csv')
+    pred_data_features = feature_data[feature_data['Unnamed: 0'] == pred_data['Unnamed'][0]]
+    
+    pred_y = loaded_model.predict(pred_data_features.drop(columns = ["Unnamed: 0"]))
+    if pred_y ==1:
+        st.write("EEG Scan shows Epileptic Seizure")
     else: 
-        st.write("you're good")
+        st.write("EEG Scan doesn't show Epilepsy")
